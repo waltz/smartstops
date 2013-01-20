ee600e97599736344de79a52bc92a7a37f14f852
CJ Bryan <christianbryan@gmail.com>
2013-01-19 16:41:22 -0800

    Respond to Twilio with a canned sms response in TwiML.

--- restapi/resources/v1/questions.js
+++ restapi/resources/v1/questions.js
@@ -1,5 +1,5 @@
 //require the Twilio module and create a REST client
-var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+//var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // This will receive a HTTP POST request that
 // contains application/x-www-form-urlencoded data. For
@@ -7,15 +7,8 @@ var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILI
 // AccountSid=AC47761615be8d2db6fcf6512360fb7815&Body=Who%27s+got+the+best+coffee+near+5545%3F&ToZip=94111&FromState=CA&ToCity=SAN+FRANCISCO&SmsSid=SM26112da2601e1ad91a7dc68c887a80c2&ToState=CA&To=%2B14154187890&ToCountry=US&FromCountry=US&SmsMessageSid=SM26112da2601e1ad91a7dc68c887a80c2&ApiVersion=2010-04-01&FromCity=SAN+FRANCISCO&SmsStatus=received&From=%2B14153074175&FromZip=94947
 
 exports.post = function(req, res, next) {
-    console.log(req.body.from);
-
-    // res.send(req.body);
-
-    client.sms.messages.post({
-        to: req.body.from,
-        from:'+14156973287',
-        body:'testing'
-    }, function(err, responseData) {
-        console.log(responseData);
-    });
+    var twilio = require('twilio');
+    var twiml = new twilio.TwimlResponse();
+    twiml.sms("thanks for texting!");
+    res.send(twiml.toString());
 }
