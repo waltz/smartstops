9b21b9011672292330a0450595dd7e9de47ade28
Ryan Closner <ryanc@codeforamerica.org>
2013-03-13 19:15:20 -0700

    Fix question.sh typo

--- test/question.sh
+++ test/question.sh
@@ -18,4 +18,4 @@ do
   esac
 done
 
-curl  -v -XPOST -H'Content-Type: application/x-www-form-urlencoded' -d'AccountSid=AC47761615be8d2db6fcf6512360fb7815&Body='"$BODY"'&ToZip=94111&FromState=CA&ToCity=SAN+FRANCISCO&SmsSid=SM26112da2601e1ad91a7dc68c887a80c2&ToState=CA&To=%2B14154187890&ToCountry=US&FromCountry=US&SmsMessageSid=SM26112da2601e1ad91a7dc68c887a80c2&ApiVersion=2010-04-01&FromCity=SAN+FRANCISCO&SmsStatus=received&From=14153074175&FromZip=94947' 'http://localhost:'"$PORT"'/questions'
+curl  -v -XPOST -H'Content-Type: application/x-www-form-urlencoded' -d'AccountSid=AC47761615be8d2db6fcf6512360fb7815&Body='"$BODY"'&ToZip=94111&FromState=CA&ToCity=SAN+FRANCISCO&SmsSid=SM26112da2601e1ad91a7dc68c887a80c2&ToState=CA&To=%2B14154187890&ToCountry=US&FromCountry=US&SmsMessageSid=SM26112da2601e1ad91a7dc68c887a80c2&ApiVersion=2010-04-01&FromCity=SAN+FRANCISCO&SmsStatus=received&From=14153074175&FromZip=94947' 'http://localhost:'"$PORT"'/api/v1/questions'
